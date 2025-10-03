67fb606d577d31f34fe86b97340cf1729c65aecf
Levkivskiydmitro <dimon65479@gmail.com>
2025-10-03 23:24:20 +0300

    task 7

--- server.js
+++ server.js
@@ -7,21 +7,18 @@ const app = express();
 const PORT = 8000;
 const HOST = 'localhost'
 
+app.use(express.json());
 
 const arrayPath = path.join(__dirname, "array.json")
-const posts = JSON.parse(fs.readFileSync(arrayPath, "utf-8"))
-
+let posts = JSON.parse(fs.readFileSync(arrayPath, "utf-8"))
 
 function Timestamp() {
     return moment().format('YYYY-MM-DD HH:mm:ss'); 
 }
 
-
 app.get('/timestamp', (req, res) => {
     res.json({ timestamp: Timestamp() });
 });
-
-
 
 app.get('/posts', (req, res) => {
     let { skip, take } = req.query;
@@ -40,35 +37,45 @@ app.get('/posts', (req, res) => {
         }
     }
     let result = posts.slice(skip);
-
     if (take !== undefined) {
         result = result.slice(0, take);
     }
-
     res.status(200).json(result);
 });
 
-
-
 app.get("/posts/:id", (req, res) => {
     const id = +req.params.id;
-    console.log(id);
-
     if (isNaN(id)) {
         res.status(400).json("need number");
         return;
     }
-
     const post = posts.find((pr) => pr.id === id);
-
     if (!post) {
         res.status(404).json("not found");
         return;
     }
-
     res.json(post);
 });
 
+app.post("/posts", async (req, res) => {
+    try {
+        const { title, description, image } = req.body;
+        if (!title || !description || !image) {
+            return res.status(422).json({ error: "Все поля (title, description, image) обязательны!" });
+        }
+        const newPost = {
+            id: posts.length + 1,
+            title,
+            description,
+            image
+        };
+        posts.push(newPost);
+        await fs.promises.writeFile(arrayPath, JSON.stringify(posts, null, 2));
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(500).json({ error: "Внутренняя ошибка сервера" });
+    }
+});
 
 app.listen(PORT, HOST , () => {
     console.log(`http://${HOST}:${PORT}`);
