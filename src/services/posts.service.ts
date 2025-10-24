b0bdd4dea826d926b4c8b559f6c20d2038d2e617
Levkivskiydmitro <dimon65479@gmail.com>
2025-10-25 01:01:07 +0300

    і

---
+++ src/services/posts.service.ts
@@ -0,0 +1,53 @@
+
+import { PrismaClient } from '@prisma/client';
+import {CreatePostChecked,UpdatePostChecked,} from '../types/post.types';
+
+const prisma = new PrismaClient();
+
+export const postsService = {
+  async getAll() {
+    try {
+      return await prisma.post.findMany();
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+      throw new Error('Error fetching posts');
+    }
+  },
+
+  async create(data: CreatePostChecked) {
+    try {
+      return await prisma.post.create({ data });
+    } catch (error) {
+      console.error('Error creating post:', error);
+      throw new Error('Error creating post');
+    }
+  },
+
+  async update(id: number, data: UpdatePostChecked) {
+    try {
+      return await prisma.post.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      console.error('Error updating post:', error);
+      throw new Error('Error updating post');
+    }
+  },
+
+  async delete(id: number) {
+    try {
+      const post = await prisma.post.delete({
+        where: { id },
+      });
+      return post;
+    } catch (error: any) {
+      if (error.code === 'P2025') {
+
+        return null;
+      }
+      console.error('Error deleting post:', error);
+      throw new Error('Error deleting post');
+    }
+  },
+};
