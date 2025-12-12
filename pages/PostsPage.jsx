0ae0b3236cdcac7b4507b1f4ce228ece1bcae5c2
Levkivskiydmitro <dimon65479@gmail.com>
2025-12-12 23:08:18 +0200

    1

---
+++ pages/PostsPage.jsx
@@ -0,0 +1,10 @@
+import PostList from "../components/PostList/PostList";
+
+export default function PostsPage() {
+  return (
+    <div style={{ padding: "10px" }}>
+      <h2>Список постов</h2>
+      <PostList />
+    </div>
+  );
+}
